8443c64949f5df258ef5666d92587d8447e02ab3
Jeremy Muhlich <jmuhlich@bitflood.org>
2019-01-18 14:21:25 -0500

    Pull common stuff into top-level module

--- ashlar/__init__.py
+++ ashlar/__init__.py
@@ -2,3 +2,7 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+from .geometry import Vector, Rectangle
+from .metadata import Tile, TileSet
+from .align import TileAlignment, register_tiles
